9153681084349f09e20887996a35480d20b414c5
JoakimBorg <info@joakimborg.se>
2024-10-23 15:46:09 +0200

    Added first draft of happiness scale

--- happiness/happiness_math.py
+++ happiness/happiness_math.py
@@ -1,17 +1,52 @@
+# Happiness scale (hs)
+# 0.5 < hs < 1 --> BOT blir gladare eftersom användaren gissar fel
+# 0 < hs < 0.5 --> BOT blir argare eftersom användaren gissar rätt
+happiness_scale = 0.5
+
+# Variabler som håller reda på rätt antal gissningar i rad.
 correct_answers_count = 0
 wrong_answers_count = 0
 
-k = 0.8
+bot_happy_exponent = 0
+bot_angry_exponent = 0
 
-def multiplier(exponent):
-  return 1 + 0.2 * k ** exponent
+# Funktioner för att kalkylera multiplikator/divisor
+def multiplier(happy_exponent, scale):
+  k_multiple = 0.7
+  multiple = 1 + 0.2 * k_multiple ** happy_exponent
 
-def divisor(exponent):
-  return 1 + 0.25 * k ** exponent
+  return round(scale * multiple, 4)
 
-m = round(multiplier(0), 4)
-d = round(divisor(0), 4)
+def divider(angry_exponent, scale):
+  k_divisor = 0.7
+  divisor = 1 + 0.25 * k_divisor ** angry_exponent
 
-print(m)
-print(0.5 * m)
-#print(0.5 / d)
+  return round(scale / divisor, 4)
+
+def happiness_math(last_answer_correct):
+  global happiness_scale
+  global bot_happy_exponent
+  global bot_angry_exponent
+
+  # Återställ exponenter när värdet rör sig över gränsen 0.5
+  if happiness_scale > 0.5:
+    bot_happy_exponent = 0
+  elif happiness_scale <= 0.5:
+    bot_angry_exponent = 0
+
+  # Sänk exponentens värde vid flera korrekta/felaktiga gissningar i rad
+  if last_answer_correct == True and bot_happy_exponent > 0:
+    bot_happy_exponent -= 1
+  elif last_answer_correct == False and bot_angry_exponent > 0:
+    bot_angry_exponent -= 1
+
+  if last_answer_correct:
+    happiness_scale = divider(bot_angry_exponent, happiness_scale)
+  elif last_answer_correct == False:
+    happiness_scale = multiplier(bot_happy_exponent, happiness_scale)
+
+  return happiness_scale
+
+x = happiness_math(False)
+
+print(x)
