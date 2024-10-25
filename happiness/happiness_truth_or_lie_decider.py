59f6be02f457d7e015640b7289f4c14cd3c43e99
JoakimBorg <info@joakimborg.se>
2024-10-25 10:48:33 +0200

    Math + truth or lie done

--- happiness/happiness_truth_or_lie_decider.py
+++ happiness/happiness_truth_or_lie_decider.py
@@ -11,28 +11,24 @@ def happiness_truth_or_lie_decider(is_last_answer_correct):
   # Gör om happiness_scale till ett värde mellan 1 och 100
   truth_or_lie_value = round(happiness_scale * 100)
 
-  # Värdemängd för sanningshalter i värde (standard är sometimes_truth)
-  always_truth = 70        # 100% chans för sanning
-  sometimes_truth = 40     # 75% chans för sanning
-  half_truth = 0           # 50% chans för sanning
+  # Värden för sanningshalter i värde (standard är sometimes_truth)
+  always_truth = 60         # 100% chans för sanning
+  sometimes_truth = 40      # 75% chans för sanning
+  half_truth = 0            # 50% chans för sanning
 
   # Procentvärden
-  percent_always = 100
-  percent_sometimes = 75
-  percent_half = 50
+  percent_always = 100      # 100%
+  percent_sometimes = 75    # 75%
+  percent_half = 50         # 50%
 
   # Slumpvärdet och procents chans för sanning (0-100)
-  percentage_chance_for_truth = sometimes_truth     # Standardprocentvärde är sometimes_truth
+  percentage_chance_for_truth = percent_sometimes     # Standardprocentvärde är sometimes_truth
   percentage_random = random.randint(0, 100)
-  
-  print('chance: ' + str(percentage_chance_for_truth))
-  print('random: ' + str(percentage_random))
-  print()
 
   # Matcha slumvärdet med procentens chans
   if truth_or_lie_value >= always_truth:
     percentage_chance_for_truth = percent_always
-  elif truth_or_lie_value >= percent_sometimes:
+  elif truth_or_lie_value >= sometimes_truth:
     percentage_chance_for_truth = percent_sometimes
   else:
     percentage_chance_for_truth = percent_half
@@ -42,14 +38,3 @@ def happiness_truth_or_lie_decider(is_last_answer_correct):
     return TRUTH
   else:
     return LIE
-
-happiness_truth_or_lie_decider(False)
-happiness_truth_or_lie_decider(False)
-happiness_truth_or_lie_decider(True)
-happiness_truth_or_lie_decider(True)
-happiness_truth_or_lie_decider(False)
-happiness_truth_or_lie_decider(False)
-happiness_truth_or_lie_decider(True)
-TL = happiness_truth_or_lie_decider(True)
-
-print(TL)
